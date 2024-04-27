58463c5fa92a7fa26bf320ea3995d7075acb3e41
SarodipDiraka <oktavor@gmail.com>
2024-04-28 00:50:09 +0300

    profile to home

--- shop/resources/views/profile/show.blade.php
+++ shop/resources/views/profile/show.blade.php
@@ -1,5 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
+        <a href="{{url('')}}"><img src="../assets/images/dark-logo.svg" alt="logo" /></a>
+        <br>
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
